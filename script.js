8ae67ac70bebc03b2f380ec79cc11e173faf0ea0
sahlaparakkal <sahlamm3@gmail.com>
2020-02-13 14:22:30 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//generate random password
+function generate(){
+
+     //set password length/complexity
+     let complexity = document.getElementById("slider").values;
+
+     //possible password values
+     let values = "ABCDEcvgh12256+*-!#";
+
+     let password = "";
+
+     //create for loop to choose password characters
+     for(var i = 0; i<= complexity; i++){
+         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+
+     }
+
+     //add password to textbox/display area
+     document.getElementById("displsay").value = password;
+
+     //add password to previously generated passwords section
+     document.getElementById("lastNums").innerHTML += "<br  />";
+
+}
+
+//set default length display of 25
+document.getElementById("length").innerHTML = "Length: 25";
+
+//function to set length based on slider position
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length:" + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+}
+
+//function to copy password to clipboard
+function copyPassword(){
+
+    document.getElementById("display").querySelector();
+
+    document.execCommand("copy");
+
+    alert("password copied to clipboard!");
+}
